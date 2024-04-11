0f257213da1403234497050f2173c2f91589919a
hugomers <hugomo0897@gmail.com>
2024-04-10 18:14:23 -0600

    se crean canales preventa y resurtido

--- index.js
+++ index.js
@@ -8,11 +8,12 @@ const io = new Server({cors:{ origin:allowedSites }});
 
 require('./preventa.js')(io)
 require('./resurtido.js')(io)
+
 io.on('connection', (socket) => {
     console.log(socket.id);
     socket.on('disconnect', () => {
       console.log('user disconnected');
     });
   });
-  
+
 io.listen(3000);
