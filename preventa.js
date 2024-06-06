810fe536b5836c49eb43f820744caa867f74a9ca
hugomers <hugomo0897@gmail.com>
2024-06-05 19:55:42 -0600

    se esta modificando el archivo de preventa

--- preventa.js
+++ preventa.js
@@ -1,9 +1,29 @@
 module.exports = (io) => {
     const preventa = io.of('/preventa')
     console.log('si se importo preventa')
-    // console.log(preventa)
-    preventa.on('connection', socket => {
-        console.log(socket.id)
-        console.log('conexion desde preventa')
-    })
+    preventa.on('connection', (pvt) => {
+        console.log(`Conexion Id ${pvt.id}`);
+        console.log(`conexion desde preventa ${pvt.id}`)
+
+        pvt.on('ParametrosConexion', (param) => {
+            const room = param.join //creacion de room 
+            const cuenta = param.account.nick //cuenta conectada
+            pvt.join(room);
+
+            console.log(`usuario ${cuenta} ingreso al room ${room}`)
+
+            pvt.in(room).allSockets().then((sockets) => {
+                const users = Array.from(sockets);
+                preventa.to(room).emit('updateUserList', users);
+            });
+
+            pvt.on('CreacionPedido', (parametros) => {
+                console.log(parametros._store)
+                preventa.to(room).emit('PedidoCreado',parametros)
+            })
+        })
+        pvt.on('disconnect',() =>{
+            console.log('medesconecte');
+        })
+    }) 
 }
